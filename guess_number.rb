91c418356c9c90596a19b85729a80d4948a8cdfa
Kate Petrova <krastina.petrova@gmail.com>
2017-01-26 20:48:45 -0700

    Kate's version

--- guess_number.rb
+++ guess_number.rb
@@ -1,1 +1,15 @@
-# guess the number!
+my_num = rand(1..10)
+guess = 0
+puts "The nubmer is #{my_num}"
+puts "Guess the number!"
+while my_num != guess
+guess = gets.chomp
+guess = guess.to_i
+if guess < my_num
+    puts "Too low! Guess again."
+elsif guess > my_num
+    puts "Too high! Guess again."
+else
+    puts "You got it!"
+end
+end
